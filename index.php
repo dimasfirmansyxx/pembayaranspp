133f9721f7f1c7504cf4059c127afd59d2c5521d
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-03-07 10:37:42 +0700

    create beranda page

--- index.php
+++ index.php
@@ -14,72 +14,11 @@ include 'templates/nav.php';
 <div class="content-section mt-5">
 	<div class="card">
 		<div class="card-header bg-warning text-white">
-			{CARD TITLE}
+			Beranda
 		</div>
 		<div class="card-body">
-			<h3>Button</h3>
-			<button class="btn btn-primary">Primary</button>
-			<button class="btn btn-danger">Danger</button>
-			<button class="btn btn-warning">Warning</button>
-			<button class="btn btn-success">Success</button>
-
-			<h3 class="mt-5">Table</h3>
-			<div class="table-responsive">
-				<table class="table">
-					<thead class="bg-warning text-white">
-						<tr>
-							<th>#</th>
-							<th>Name</th>
-							<th>Age</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>John Doe</td>
-							<td>34</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Jane Doe</td>
-							<td>32</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-
-			<h3 class="mt-5">Forms</h3>
-			<div class="form-group">
-				<label>Input</label>
-				<input type="text" class="form-control" value="value">
-			</div>
-			<div class="form-group">
-				<label>Combobox</label>
-				<select class="form-control">
-					<option>Option 1</option>
-					<option>Option 2</option>
-					<option>Option 3</option>
-				</select>
-			</div>
-			<div class="form-group">
-				<label>Textarea</label>
-				<textarea class="form-control"></textarea>
-			</div>
-
-			<h3 class="mt-5">Alert</h3>
-			<div class="alert bg-primary text-white">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-			</div>
-			<div class="alert bg-danger text-white">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-			</div>
-			<div class="alert bg-warning text-white">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-			</div>
-			<div class="alert bg-success text-white">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-			</div>
-
+			<h1 class="text-center"><?= $myfunc->siteinfo("nama_sekolah") ?></h1>
+			<h3 class="text-center"><?= $myfunc->siteinfo("alamat") ?></h3>
 		</div>
 	</div>
 </div>
--- templates/nav.php
+++ templates/nav.php
@@ -1,16 +1,21 @@
 <!-- navbar -->
 <nav class="navbar bg-warning">
 	<h3 class="text-center text-white mt-5 mb-5">Pembayaran</h3>
-	<a href="#">HOME</a>
-	<a href="#">MENU 1</a>
-	<a href="#">MENU 2</a>
-	<a href="#">MENU 3</a>
+	<a href="<?= $myfunc->baseurl ?>">Beranda</a>
+	<a href="<?= $myfunc->baseurl ?>student.php">Manajemen Siswa</a>
+	<a href="<?= $myfunc->baseurl ?>class.php">Manajemen Kelas</a>
+	<a href="<?= $myfunc->baseurl ?>major.php">Manajemen Jurusan</a>
+	<a href="<?= $myfunc->baseurl ?>payment.php">Manajemen Pembayaran</a>
+	<a href="<?= $myfunc->baseurl ?>officer.php">Manajemen Petugas</a>
+	<a href="<?= $myfunc->baseurl ?>transaction.php">Transaksi</a>
+	<a href="<?= $myfunc->baseurl ?>history.php">History Transaksi</a>
+	<a href="<?= $myfunc->baseurl ?>report.php">Laporan</a>
 </nav>
 
 <!-- topnav -->
 <nav class="topnav bg-dark">
-	<a href="#">Profil</a>
-	<a href="#">Logout</a>
+	<a href="<?= $myfunc->baseurl ?>profil.php">Profil</a>
+	<a href="<?= $myfunc->baseurl ?>index.php?logout=">Logout</a>
 </nav>
 
 	<div class="content">
@@ -25,4 +30,10 @@
 			<p><?= $myfunc->subtitle ?></p>
 		</div>	
 
+		<?php if ( isset($_SESSION["flash_data"]) ): ?>
+			<div class="alert bg-<?= $_SESSION["flash_data"]["status"] ?> text-white">
+				<?= $_SESSION["flash_data"]["message"] ?>
+			</div>
+			<?php unset($_SESSION["flash_data"]) ?>
+		<?php endif ?>
 		<!-- content section -->
